8be57075b8ec297c9ecc559e50c1c231423fc365
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-02 14:35:53 -0700

    finished closure assignments

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -55,7 +55,7 @@ function contains(item, list, cb) {
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array,index, cb) {
+function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
